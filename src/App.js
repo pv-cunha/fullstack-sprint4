f3592bfd255109f99f25d6b716bfef9baa1b9558
Paulo Victor <pvcunha05@gmail.com>
2021-06-26 11:12:44 -0300

    Json Server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <div>Funcionando</div>;
+  const [test, setTest] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('/products')
+      .then((r) => r.json())
+      .then((json) => setTest(json));
+  }, []);
+
+  console.log(test);
+
+  return (
+    <div>
+      {test && test.map((t) => <img src={t.image} alt={t.sku} key={t.sku} />)}
+    </div>
+  );
 };
 
 export default App;

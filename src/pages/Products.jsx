69e6f27bc0aeff340b4fdd7b8d32d12fdb9365dc
Paulo Victor <pvcunha05@gmail.com>
2021-06-27 15:56:49 -0300

    Hook de fetch

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import styles from './Products.module.css';
 import ProductItem from '../components/productItem/ProductItem';
+import Loading from '../components/loading/Loading';
+import Alert from '../components/alert/Alert';
+import useFetch from '../hooks/useFetch';
 
-const Products = (props) => {
-  const [products, setProducts] = React.useState(null);
+const Products = () => {
+  const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
-    fetch('/products')
-      .then((r) => r.json())
-      .then((json) => {
-        const data = json.slice(0, json.length - 1);
-
-        setProducts(data);
-      });
-  }, []);
-
-  console.log(products);
+    request('/products');
+  }, [request]);
 
   return (
     <section className={`${styles.products} container`}>
-      {products &&
-        products.map((product) => (
-          <ProductItem
-            key={product.sku}
-            imageUrl={product.image}
-            description={product.name}
-            price={product.price}
-            sku={product.sku}
-          />
-        ))}
+      {loading && <Loading />}
+      {error && <Alert />}
+
+      {data &&
+        data
+          .slice(0, data.length - 1)
+          .map((product) => (
+            <ProductItem
+              key={product.sku}
+              imageUrl={product.image}
+              description={product.name}
+              price={product.price}
+              sku={product.sku}
+            />
+          ))}
     </section>
   );
 };
